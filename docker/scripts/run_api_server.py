a67e4c5a467933b6fc660fd06644eafb6e2aee90
alexanderlukens <alexander.lukens1@gmail.com>
2023-08-09 16:54:52 -0700

    SETUP - get fast api server running

--- docker/scripts/run_api_server.py
+++ docker/scripts/run_api_server.py
@@ -3,6 +3,4 @@ import uvicorn
 if __name__ == "__main__":
     host = "0.0.0.0"
     port = 80
-    uvicorn.run(
-        host=host, port=port, app="api.main:app", reload=True, log_level="debug"
-    )
+    uvicorn.run(host=host, port=port, app="src.api:app", reload=True, log_level="debug")
