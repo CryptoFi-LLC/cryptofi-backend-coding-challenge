9a41b1ba59a3073888b9d28242ff2180891cd6dc
alexanderlukens <alexander.lukens1@gmail.com>
2023-08-10 12:28:19 -0700

    SETUP - create tables in dynamodb

--- docker/scripts/setup_db_tables.py
+++ docker/scripts/setup_db_tables.py
@@ -1,6 +1,16 @@
+from src.model import RecurringOrder, User
+
+
 class DbSetup:
     def run(self):
-        pass
+        self._create_recurring_order_table()
+        self._create_user_table()
+
+    def _create_recurring_order_table(self):
+        RecurringOrder.create_table()
+
+    def _create_user_table(self):
+        User.create_table()
 
 
 if __name__ == "__main__":
---
+++ src/model.py
@@ -0,0 +1,30 @@
+import os
+from abc import abstractmethod
+from dyntastic import Dyntastic
+from pydantic import Field
+
+
+class DynamoDbModelBase(Dyntastic):
+    __table_region__ = os.environ.get("REGION_NAME")
+    __table_host__ = os.environ.get("DYNAMO_ENDPOINT")
+    __hash_key__ = "hash_key"
+    __range_key__ = "range_key"
+
+    @property
+    @abstractmethod
+    def __table_name__(self):  # over-ride this on each implementing class
+        pass
+
+    # Fields that every DynamoDB item shall have
+    # the key fields are marked optional so the object can be created by the dev
+    # on new records keys set by the init method to conform with the class partition and sort keys
+    hash_key: str = Field(default=None, title="DynamoDB Partition Key")
+    range_key: str = Field(default=None, title="DynamoDB Sort Key")
+
+
+class User(DynamoDbModelBase):
+    __table_name__ = "User"
+
+
+class RecurringOrder(DynamoDbModelBase):
+    __table_name__ = "RecurringOrder"
